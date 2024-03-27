b1d18852d0ef29fd7d26b4bb0391d5ee8bae8124
swapnil sharma <149245468+Swapnilsharma0-o@users.noreply.github.com>
2024-03-27 12:54:58 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,30 @@
+#1 Accept two numbers from the user and print 
+# a) addition 
+# b) first number squared 2 
+# c) first number raised to number second number
+
+# no1 = int(input("enter no 1:"))
+# no2 = int(input("enter no 2:"))
+# print("addition :",int(no1 + no2))
+# print("first number squared :",no1 **2)
+# print("first number raised to number second number :",no1 ** no2)
+
+#2 Accept String from user and output upper case of the input string 
+str1 = str(input("enter string:"))
+uc = str1.upper()   
+print(uc)
+print("lower : " ,str1.lower()) 
+
+
+#3 Define a variable named "raise_salary_percentage" and get the salary raise 
+# percentage from the user, Now apply the raise to an employee
+# with harcoded data Name= 'gaurav' existing_salary = 900 INR and 
+# print the incremented salary to the console
+
+# vname = int(input("enter salary :"))
+# per = int(input("enter percentage:"))
+# sal = ((vname // per) *100)
+# print(sal)
+
+# 4) Get the height from the user in cms and display the user height back to console
+# in foot/feet and inches
